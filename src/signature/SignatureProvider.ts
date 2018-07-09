e5ffabfa86ccdb0a294750d037ee54efec068aa5
Linus Norton <linusnorton@gmail.com>
2018-07-09 21:28:02 +0100

    corrected some issues with the hashing

--- src/signature/SignatureProvider.ts
+++ src/signature/SignatureProvider.ts
@@ -1,11 +1,10 @@
 import {Logger} from "pino";
-import {Utils} from "web3/types";
 
 export class SignatureProvider {
 
   constructor(
-    private readonly account: EthereumAccount,
-    private readonly utils: Utils,
+    private readonly account: EthAccount,
+    private readonly utils: EthUtils,
     private readonly logger: Logger
   ) { }
 
@@ -13,11 +12,10 @@ export class SignatureProvider {
    * Hash and sign the parameters using the ethereum account
    */
   public sign(id: string, price: string, expiry: number): string {
-    const params = [id, price, expiry].join("_");
-    const hash = this.utils.soliditySha3(params);
+    const hash = this.utils.soliditySha3(toBytes32(id), "_", price, "_", expiry);
     const signedData = this.account.sign(hash);
 
-    this.logger.debug(`hash(${params}) = ${hash}`);
+    this.logger.debug(`hash(${toBytes32(id)}, "_", ${price}, "_", ${expiry}) = ${hash}`);
     this.logger.debug(`sign(${hash}) = ${signedData.signature}`);
 
     return signedData.signature;
@@ -25,8 +23,12 @@ export class SignatureProvider {
 
 }
 
-export interface EthereumAccount {
+export interface EthAccount {
   sign: (data: string) => Signature;
+}
+
+interface EthUtils {
+  soliditySha3: (...args: Array<string | number>) => string
 }
 
 interface Signature {
@@ -36,4 +38,17 @@ interface Signature {
   s: string,
   v: string,
   signature: string
+}
+
+/**
+ * Convert a string to a hexidecimal string padded to 32 bytes
+ */
+function toBytes32(str) {
+  let hex = '0x';
+
+  for (let i = 0; i < 32; i++) {
+    hex += str.length > i ? str.charCodeAt(i).toString(16) : '00';
+  }
+
+  return hex;
 }
